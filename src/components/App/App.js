024c09519d6b2f9d728081ffef9722847eb024d4
Kensya <himemusou@gmail.com>
2023-09-26 09:48:14 +0300

    React 2.2.14

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
-import { Component } from 'react';
-import { Layout, Pagination } from 'antd';
+import { Component, Fragment } from 'react';
+import { Layout, Pagination, Alert } from 'antd';
+import { Offline, Online } from 'react-detect-offline';
 
 const { Header, Footer } = Layout;
 
@@ -7,31 +8,85 @@ import '../../rest.scss';
 import './App.scss';
 
 import Navigation from '../Navigation';
-// import { getMoviesList } from '../../services/moviesapi.js';
 
 export default class App extends Component {
   render() {
+    const connectionIssues = (
+      <Fragment>
+        <p>
+          Sorry, but it looks like there is no internet connection available at the
+          moment. Here are a few recommendations to troubleshoot the issue:
+        </p>
+        <ol style={{ padding: '15px 0' }}>
+          <li>
+            Check your Wi-Fi or Ethernet connection to ensure it is properly
+            connected and functional.
+          </li>
+          <li>Restart your modem or router to refresh the network connection.</li>
+          <li>
+            Try connecting to a different Wi-Fi network or using a wired Ethernet
+            connection.
+          </li>
+          <li>
+            Ensure that airplane mode is turned off on your device, as it may disable
+            network connectivity.
+          </li>
+          <li>
+            Verify if other devices are able to connect to the internet. If they can,
+            the issue may be specific to your device.
+          </li>
+          <li>
+            Contact your Internet Service Provider (ISP) to check for any service
+            outages or technical issues in your area.
+          </li>
+        </ol>
+        <p>
+          Hopefully, these steps will help you resolve the internet connection
+          problem. Let me know if you need further assistance!
+        </p>
+      </Fragment>
+    );
+
     return (
-      <Layout className="app">
-        <Header className="app__header">
-          <Navigation />
-        </Header>
-        <Footer
-          style={{
-            padding: '35px 50px',
-            textAlign: 'center',
-            backgroundColor: '#fff',
-          }}
-        >
-          <Pagination
-            onChange={() => {
-              console.log('Click');
+      <Layout
+        style={{
+          height: '100vh',
+          margin: '0 auto',
+          maxWidth: '1145px',
+          backgroundColor: '#fff',
+        }}
+      >
+        <Online>
+          <Header style={{ height: 'auto', backgroundColor: '#fff' }}>
+            <Navigation />
+          </Header>
+          <Footer
+            style={{
+              padding: '35px 50px',
+              textAlign: 'center',
+              backgroundColor: '#fff',
             }}
-            defaultCurrent={1}
-            pageSize={6}
-            total={20}
+          >
+            <Pagination
+              onChange={() => {
+                console.log('Click');
+              }}
+              defaultCurrent={1}
+              pageSize={6}
+              total={20}
+            />
+          </Footer>
+        </Online>
+
+        <Offline>
+          <Alert
+            message="No internet connection"
+            description={connectionIssues}
+            type="error"
+            showIcon
+            style={{ maxWidth: '935px', margin: 'auto' }}
           />
-        </Footer>
+        </Offline>
       </Layout>
     );
   }
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,7 +1,7 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import { format } from 'date-fns';
 import { nanoid } from 'nanoid';
-import { Layout, Space } from 'antd';
+import { Layout, Space, Empty, Alert } from 'antd';
 const { Content } = Layout;
 
 import './MoviesList.scss';
@@ -12,14 +12,43 @@ import { debounce, getMoviesList } from '../../services/moviesapi.js';
 export default class MoviesList extends Component {
   state = {
     movies: [],
+    hasSearched: false,
+    hasFound: false,
+    hasError: false,
+    errorMessage: '',
+    totalResults: 0,
+    totalPages: 0,
   };
 
   handleSearchQuery = debounce((query) => {
-    getMoviesList(query).then((data) =>
+    getMoviesList(query).then((data) => {
+      // Если данные не получены от сервера, выходим с отметкой об ошибке.
+      if (typeof data !== 'object') {
+        this.setState({
+          hasError: true,
+          errorMessage: data,
+        });
+        return;
+      }
+      // Распихиваем полученные данные по стейту.
       this.setState({
+        hasSearched: true,
         movies: [...data.results],
-      })
-    );
+        totalResults: data.total_results,
+        totalPages: data.total_pages,
+      });
+      // Если текущий поиск не дал результатов, выходим с отметкой о безуспешном поиске.
+      if (data.total_results === 0) {
+        this.setState({
+          hasFound: false,
+        });
+        return;
+      }
+      // Поиск успешен, завершаем выполнение и делаем отметку в стейте.
+      this.setState({
+        hasFound: true,
+      });
+    });
   }, 650);
 
   setReleaseDate = (releaseDate) => {
@@ -40,12 +69,13 @@ export default class MoviesList extends Component {
     return croppedText.join(' ');
   };
 
-  render() {
-    const moviesList = this.state.movies.map((movie) => {
+  renderMoviesList = (movies) => {
+    return movies.map((movie) => {
       return (
         <MovieItem
           key={nanoid()}
-          title={this.setTruncateText(movie.title, 3)}
+          shortTitle={this.setTruncateText(movie.title, 3)}
+          fullTitle={movie.title}
           coverPath={movie.poster_path}
           avgRating={movie.vote_average.toFixed(1)}
           release={this.setReleaseDate(movie.release_date)}
@@ -53,11 +83,71 @@ export default class MoviesList extends Component {
         />
       );
     });
+  };
+
+  render() {
+    // Исходное состояние компонента, поиск не был произведен.
+    const initialMoviesList = (
+      <Fragment>
+        <Empty
+          style={{
+            height: '500px',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        />
+        {/* Ничего не найдено, список фильмов пуст. */}
+        {this.state.hasSearched && !this.state.hasFound ? (
+          <Alert
+            message="Not found"
+            description="No movies were found matching your request."
+            type="info"
+            showIcon
+            style={{ width: '375px', position: 'absolute', top: '15%' }}
+          />
+        ) : null}
+        {/* Сервер не ответил или данные пришли с ошибкой. */}
+        {this.state.hasError ? (
+          <Alert
+            message="Server not responce"
+            description={`No response was received from the server. Error message: ${this.state.errorMessage}`}
+            type="error"
+            showIcon
+            style={{ width: '375px', position: 'absolute', top: '15%' }}
+          />
+        ) : null}
+      </Fragment>
+    );
+
+    // Рендерим список найденого.
+    const foundMoviesList = (
+      <Space
+        style={{
+          paddingTop: '35px',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          gap: '35px',
+        }}
+      >
+        {this.renderMoviesList(this.state.movies)}
+      </Space>
+    );
 
     return (
-      <Content className="app__content content">
+      <Content
+        style={{
+          maxWidth: '935px',
+          margin: '0 auto',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <SearchPanel inputValue="" handleSearchQuery={this.handleSearchQuery} />
-        <Space className="content__movieslist">{moviesList}</Space>
+        {this.state.hasFound ? foundMoviesList : initialMoviesList}
       </Content>
     );
   }
